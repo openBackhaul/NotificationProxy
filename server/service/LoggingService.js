31a1fdf91d244bc2d16018d0aa6b460a321b7730
Martin Fuchs <m.fuchs@mentopolis.de>
2023-11-22 14:43:25 +0100

    add LoggingService.js

---
+++ server/service/LoggingService.js
@@ -0,0 +1,24 @@
+const pino = require('pino');
+
+// configuration for pino with pretty console output and logfile output
+const transports = pino.transport({
+  targets: [
+    {
+      level: 'info',
+      target: 'pino-pretty',
+      options: { colorize: true }
+    },
+    {
+      level: 'trace',
+      target: 'pino/file',
+      options: { destination: 'logs/NotificationProxy.log', mkdir: true }
+    }
+  ]
+});
+
+// create pino logger instance
+const logger = pino(transports);
+
+exports.getLogger = function getLogger() {
+  return logger;
+};
